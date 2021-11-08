58c29498e770b2bcf1c8e341f14245dd685d4175
Antonin <antoninfouquet.pro@gmail.com>
2021-11-08 16:52:02 +0100

    Début tests anto

---
+++ Bouquet de Pneu/Assets/Scripts/GrowFlower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowFlower : MonoBehaviour
+{
+    public float cooldown;      //Durée fixe du cooldown
+    private float nextFlower;   //Prochain timing où on peut agir
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
