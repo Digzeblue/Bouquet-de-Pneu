df5751cb3c48cd299142b2f30014ae8fae1da6f7
Antonin <antoninfouquet.pro@gmail.com>
2021-11-08 17:32:12 +0100

    spawn des fleurs (v1)

--- Bouquet de Pneu/Assets/Scripts/GrowFlower.cs
+++ Bouquet de Pneu/Assets/Scripts/GrowFlower.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class GrowFlower : MonoBehaviour
 {
+    public float radius;        //Rayon planète
+
+    public GameObject player;
+    public GameObject flower;   //fleur à créer
+
     public float cooldown;      //Durée fixe du cooldown
     private float nextFlower;   //Prochain timing où on peut agir
+
+    private Vector3 center;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        center = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if(Time.time > nextFlower)  //Si le cooldown est dépassé
+            {
+                nextFlower = Time.time + cooldown;  //reset timer
+
+                Vector3 pos = player.transform.position;
+                Quaternion rot = Quaternion.FromToRotation(Vector3.up, center - pos);
+                Instantiate(flower, pos, rot);
+            }
+        }
     }
 }
